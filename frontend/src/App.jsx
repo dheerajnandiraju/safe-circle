6ac08604c5ccf606bed4c9916d65156d4d925a96
Rohith Singh <singhmalik583@gmail.com>
2024-02-23 17:23:43 +0530

    "navbars"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CommunityPage from "./pages/community/community";
 import LocationPage from "./pages/location/location";
 import HomePage from "./pages/home-page";
+import Navbar from "./components/home-components/navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+      <div className="h-16" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/community" element={<CommunityPage />} />
--- frontend/src/pages/community/community.jsx
+++ frontend/src/pages/community/community.jsx
@@ -1,7 +1,13 @@
+import SideBar from "@/components/community-components/sidebar";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const CommunityPage = () => {
-  return <div>CommunityPage</div>;
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
 };
 
 export default CommunityPage;
